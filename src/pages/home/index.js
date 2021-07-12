cf74c277c5a0bc541e5bb5e26c7f05951ae2c3fe
macedoluc <lucas.m.pires@estudante.firjan.senai.br>
2021-07-12 14:58:28 -0300

    atualizar input

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
@@ -8,15 +8,15 @@ import {
   FlatList,
   StyleSheet,
   Image,
-  ScrollView
+  ScrollView,
+  ActivityIndicator,
 } from 'react-native';
 import Input from '../../components/input';
 import Botao from '../../components/button';
 import Header from '../../components/header';
 import Styles from './style';
-import axios from 'axios'
-import { Appbar } from 'react-native-paper';
-
+import axios from 'axios';
+import {Appbar} from 'react-native-paper';
 
 const Home = () => {
   const [nome, setNome] = useState();
@@ -24,72 +24,70 @@ const Home = () => {
   const [num, setNum] = useState(0);
   const [categoria, setCategoria] = useState([]);
   const [mostrar, setMostrar] = useState(true);
-
-
-
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getProdutos();
   }, [setProduto]);
 
   function aumentarNum() {
-    setNum(num + 1)
+    setNum(num + 1);
   }
 
-  getProdutos = (num) => {
-    axios.get(`https://ecommerceflascododragao.herokuapp.com/produtos`)
+  getProdutos = num => {
+    setIsLoading(true);
+    axios
+      .get(`https://ecommerceflascododragao.herokuapp.com/produtos`)
       // Vamos ter que trabalhar no page e no size ()=>getP
-      .then((response) => {
+      .then(response => {
         //console.log(response.data);
         setProduto(response.data);
-      }).catch(function (error) {
+        setIsLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setIsLoading(false);
+      });
+  };
+  function listarAirline() {
+    console.log(nome);
+    setMostrar(false);
+    axios
+      .get(`https://ecommerceflascododragao.herokuapp.com/produtos`)
+      .then(response => {
+        for (var i = 0; i < response.data.length; i++) {
+          if (response.data[i].categoria.nome == nome) {
+            console.log(response.data[i]);
+            setCategoria(response.data[i]);
+            console.log(categoria);
+          }
+        }
+      })
+      .catch(function (error) {
         console.log(error);
       });
-
   }
-  function listarAirline() {
-    console.log(nome)
-    setMostrar(false);
-    axios.get(`https://ecommerceflascododragao.herokuapp.com/produtos`).then((response) => {
-
-      for (var i = 0; i < response.data.length; i++) {
-        if (response.data[i].categoria.nome == nome) {
-          console.log(response.data[i])
-          setCategoria(response.data[i]);
-          console.log(categoria);
-        }
-      }
-
-    }).catch(function (error) {
-      console.log(error);
-    });
-  }
-
-  function MyComponent() {
-    return (
-
-      <Appbar.Header style={Styles.busca}>
-        <Appbar.Content />
-        <Input
-          texto={[nome, setNome]}
-        />
-        <Appbar.Action icon="magnify" onPress={listarAirline} />
-      </Appbar.Header>
-    )
-  };
 
   return (
     <SafeAreaView>
-      <Header/>
-      <MyComponent />
+      <Header />
+      <Appbar.Header style={Styles.busca}>
+        <Appbar.Content />
+        <Input texto={[nome, setNome]} />
+        <Appbar.Action icon="magnify" onPress={listarAirline} />
+      </Appbar.Header>
 
-      {mostrar &&
-
+      {isLoading ? (
+        <View>
+          <ActivityIndicator size="large" color="#5500dc" />
+        </View>
+      ) : (
+        // {mostrar &&
         <FlatList
           onScrollBeingDrag={aumentarNum}
           data={produto}
           keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => (
+          renderItem={({item}) => (
             <View>
               <Text>Nome: {item.nome}</Text>
               <Text>Valor: {item.valorUnitario}</Text>
@@ -98,41 +96,41 @@ const Home = () => {
                 style={{
                   width: 300,
                   height: 100,
-                  resizeMode: 'contain'
+                  resizeMode: 'contain',
                 }}
                 source={{
                   uri: item.url,
-                }} />
-            </View>
-          )}
-        />}
-
-      {!mostrar &&
-        < FlatList
-          onScrollBeingDrag={aumentarNum}
-          data={categoria}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => (
-            <View>
-              <Text>Nome: {item.nome}</Text>
-              <Text>Valor: {item.valorUnitario}</Text>
-              <Text>Categoria: {item.categoria.nome}</Text>
-              <Image
-                style={{
-                  width: 300,
-                  height: 100,
-                  resizeMode: 'contain'
                 }}
-                source={{
-                  uri: item.url,
-                }} />
+              />
             </View>
           )}
-        />}
+        />
+      )}
 
-    </SafeAreaView >
+      {/* {!mostrar &&
+      < FlatList
+        onScrollBeingDrag={aumentarNum}
+        data={categoria}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>Nome: {item.nome}</Text>
+            <Text>Valor: {item.valorUnitario}</Text>
+            <Text>Categoria: {item.categoria.nome}</Text>
+            <Image
+              style={{
+                width: 300,
+                height: 100,
+                resizeMode: 'contain'
+              }}
+              source={{
+                uri: item.url,
+              }} />
+          </View>
+        )}
+      />} */}
+    </SafeAreaView>
   );
 };
-
 
 export default Home;
