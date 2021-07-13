08b32e52ca8b692319d6b195b220a7e18150f0ea
VanessaWendling <vanessamw2001@hotmail.com>
2021-07-12 21:06:03 -0300

    carrinho bonitinho

--- src/pages/carrinho/index.js
+++ src/pages/carrinho/index.js
@@ -5,11 +5,19 @@ import {
   SafeAreaView,
   TouchableHighlight,
   FlatList,
+  Image,
+  ScrollView
+
 } from 'react-native';
 import Botao from '../../components/button';
 import Header from '../../components/header';
 import CarrinhoContext from '../../context/CarrinhoContext';
 import styles from './style';
+
+function handlePress() {
+  alert("Sua compra foi realizada com sucesso");
+}
+
 
 const Carrinho = () => {
   const context = useContext(CarrinhoContext);
@@ -18,32 +26,39 @@ const Carrinho = () => {
   const valorTotal = context.produtos
     .reduce((total, prod) => total + prod.item.valorUnitario, 0)
     .toFixed(2);
+
   return (
     <SafeAreaView>
-      <Header />
-      <Text style={styles.title}>Itens no Carrinho</Text>
-      <FlatList
-        data={context.produtos}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item, index }) => {
-          return (
-            <View style={styles.rowcontainer}>
-              <Text style={styles.text}>Nome: {item.item.nome}</Text>
-              <Text style={styles.text}>Valor: {item.item.valorUnitario}</Text>
-            </View>
-          );
-        }}
-      />
-      <View>
-        <Text style={styles.total}>Total: {valorTotal}</Text>
-      </View>
-      <View>
-        <Botao
-          // style={stylesbutton.button}
-          title={'Comprar'}
-          onPress={() => { }}></Botao>
-      </View>
+      <ScrollView>
+        <Header />
+        <Text style={styles.title}>Itens no Carrinho</Text>
+        <FlatList
+          data={context.produtos}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => {
+            return (
+              <View style={styles.listItem}>
+                <Image style={styles.productImage} source={{ uri: item.item.url }} />
+                <View style={styles.productInfo}>
+                  <Text>Nome: {item.item.nome}</Text>
+                  <Text>Valor: {item.item.valorUnitario}</Text>
+                  <Text>Categoria: {item.item.categoria.nome}</Text>
+                  <Text>Descrição: {item.item.descricao}</Text>
+                </View>
+              </View>
+            );
+          }}
+        />
+        <View>
+          <Text style={styles.total}>Total: {valorTotal}</Text>
+        </View>
+
+        <Botao title={'Comprar'}
+          onPress={handlePress}></Botao>
+
+      </ScrollView>
     </SafeAreaView>
   );
-};
+}
+
 export default Carrinho;
